ec886c8a2f0c0abc2eb1b0d71596aca10fefffbf
isa95330 <isabellemazecoiffure@gmail.com>
2023-12-18 14:39:50 +0100

    components.2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,35 @@
+// PokemonCard.jsx
+
 import React from 'react';
 
 const PokemonCard = () => {
+  // Étape 2: Ajouter des données Pokémon
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  // Étape 3: Rendre le composant dynamique
+  const pokemon = pokemonList[0]; // Changer l'index pour sélectionner un autre Pokémon
+
+  // Étape 4: Affichage de l'image
+  const imageElement = pokemon.imgSrc ? (
+    <img src={pokemon.imgSrc} alt={pokemon.name} />
+  ) : (
+    <p>???</p>
+  );
+
+  // Affichage du composant PokemonCard
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
+      {imageElement}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 };

5d1a46a536c9783ad51b7634b58ff876091b2aec
isa95330 <isabellemazecoiffure@gmail.com>
2023-12-18 15:09:07 +0100

    props.1

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 import PokemonCard from './components/PokemonCard';
 
 const App = () => {
+  // Déplacer le tableau pokemonList vers le composant App
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  // Transmettre un Pokémon à PokemonCard via une prop
+  const pokemon = pokemonList[0]; // Choisissez le Pokémon que vous souhaitez transmettre
+
   return (
     <div>
-      <PokemonCard />
+      {/* Utilisation du composant PokemonCard avec la prop pokemon */}
+      <PokemonCard pokemon={pokemon} />
     </div>
   );
 };
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,22 +1,12 @@
-// PokemonCard.jsx
-
 import React from 'react';
 
-const PokemonCard = () => {
-  // Étape 2: Ajouter des données Pokémon
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+// Ajoute un paramètre props à la fonction PokemonCard
+const PokemonCard = (props) => {
+  // Utilise console.log(props) pour afficher l'objet props dans la console
+  console.log(props);
 
   // Étape 3: Rendre le composant dynamique
-  const pokemon = pokemonList[0]; // Changer l'index pour sélectionner un autre Pokémon
+  const { pokemon } = props; // Utilise la prop pour obtenir le Pokémon
 
   // Étape 4: Affichage de l'image
   const imageElement = pokemon.imgSrc ? (
